72ae1ff228609f5d06fef7994b05b17bbf379c41
marrcelp <marcel.piaszczyk@gmail.com>
2024-07-26 10:09:17 +0200

    Resolved exercise 3.1

--- 03/app.js
+++ 03/app.js
@@ -1,18 +1,35 @@
 const min = 1;
 const max = 100;
 
-const a = randomNumber(min, max);
-const b = randomNumber(min, max);
-const c = randomNumber(min, max);
+const a = parseInt(randomNumber(min, max));
+const b = parseInt(randomNumber(min, max));
+const c = parseInt(randomNumber(min, max));
 
 console.log(a, b, c);
-
-
-
-
-
-
 
 function randomNumber(min, max) {
     return Math.round((Math.random() * (max - min)) + min);
 }
+
+function getSum(a, b, c) {
+    let sum;
+
+    if (a > b && b > c) {
+        sum = a + b;
+        console.log(sum);
+        return sum;
+
+    } else if (a > b && b < c) {
+        sum = a + c;
+        console.log(sum);
+        return sum;
+
+    } else  {
+        sum = b + c;
+        console.log(sum);
+        return sum;
+    }
+
+}
+
+getSum(a, b, c);
