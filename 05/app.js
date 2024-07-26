04aa7ea886e1feef28db793db3f4a22f6cd20aeb
marrcelp <marcel.piaszczyk@gmail.com>
2024-07-26 14:28:19 +0200

    resolved full exercise 5

--- 05/app.js
+++ 05/app.js
@@ -11,7 +11,45 @@ class Student {
         }
         this.grades[subject].push(grade);
     }
+
+    getSubjectAvg(subject) {
+        const subjectSum = this.grades[subject].reduce((acc, element) => acc + element);
+        const subAvg = subjectSum / this.grades[subject].length;
+        console.log(`Srednia arytmetyczna z ${subject} wynosi ${subAvg}`);
+        return subAvg;
+    }
+
+    getAvg() {
+        const allGrades = Object.values(this.grades).flat();
+        const gradesSum = allGrades.reduce((acc, element) => acc + element);
+        const gradesAvg = (gradesSum / allGrades.length).toFixed(2);
+        console.log(`Srednia wszystkich ocen ucznia wynosi ${gradesAvg}`);
+        return gradesAvg;
+    }
+
 }
 
-const marek = new Student('Marek', 'Nowak');
-console.log(marek);
+
+const student1 = new Student('Marek', 'Nowak');
+student1.addGrade('math', 4);
+student1.addGrade('math', 6);
+student1.addGrade('english', 3);
+student1.addGrade('english', 1);
+student1.addGrade('english', 5);
+student1.addGrade('english', 5);
+student1.addGrade('english', 4);
+student1.getSubjectAvg('english');
+student1.getAvg();
+
+console.log(student1)
+
+
+const student2 = new Student('Jan', 'Kowalski');
+student2.addGrade('math', 3);
+student2.addGrade('math', 5);
+student2.addGrade('english', 4);
+student2.addGrade('english', 3);
+student2.getSubjectAvg('english');
+student2.getAvg();
+
+console.log(student2)
